6eb7ae3c65fc6f1355d6bcc7a851e9c106933f74
Eric Albert <eric.albert8787@gmail.com>
2026-09-16 17:14:31 -0400

    Changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Calculator } from './calculator/calculator';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
+  standalone: true,
+  imports: [Calculator],
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('calculator-app');
+  title = 'calculator-app';
 }
